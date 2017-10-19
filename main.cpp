7dad097508d507d6c788d381a4c2a94295f67ae3
andrgolubev <andpgolubev@gmail.com>
2017-10-19 14:55:55 -0700

    Fixed straight-forward algo

--- main.cpp
+++ main.cpp
@@ -7,12 +7,33 @@
 #include <chrono>
 #include <limits>
 #include <algorithm>
+#include <map>
+#include <atomic>
+#include <thread>
 
 namespace
 {
     using vertex = std::uint64_t;
     using vertex_array = std::vector<std::uint64_t>;
     using vertex_matrix = std::vector<vertex_array>;
+    using _chrono = std::chrono::steady_clock;
+
+    struct clique
+    {
+        vertex_array m_vertices = {};
+    };
+
+
+    static vertex_matrix adjacency_matrix = {};
+    static clique optimal_clique = {};
+
+    static std::size_t m_heuristic_size = 0;
+
+    static double time_limit = 0;
+
+    volatile std::atomic<bool> should_exit(false);
+
+    static auto start_time = _chrono::now();
 
     std::vector<std::string> split(std::string& s, const std::string& delim)
     {
@@ -29,90 +50,97 @@ namespace
         return out;
     }
 
-    struct clique
-    {
-        vertex_array m_vertices = {};
-        std::size_t m_heuristic_size = 0;
-        std::size_t size()
-        {
-            auto s = m_vertices.size();
-            return s * (s - 1) / 2;
-        }
-    };
-
-
-    static vertex_matrix adjacency_matrix = {};
-    static clique optimal_clique = {};
-
-    vertex_array find_candidates(vertex v)
+    inline vertex_array get_connected(vertex v)
     {
         vertex n_vertices = static_cast<vertex>(adjacency_matrix.size());
         const auto& row = adjacency_matrix[v];
         vertex_array C = {};
-        for (vertex i = v + 1; i < n_vertices; ++i)
+        // TODO: verify if can calculate from v + 1:
+        for (vertex i = 0; i < n_vertices; ++i)
         {
             if (row[i] > 0) { C.push_back(i); }
         }
         return C;
     }
 
-    std::uint64_t upper_bound(const clique& Q, const vertex_array& C)
+    vertex_array find_candidates(const vertex_array& vertices)
     {
-        return std::numeric_limits<std::uint64_t>::max();
-    }
-
-    vertex_array intersection(const vertex_array& c1, const vertex_array& c2)
-    {
-        vertex_array out = {};
-        auto s1 = c1.size(), s2 = c2.size();
-        const auto& c_min = s1 < s2 ? c1 : c2;
-        const auto& c_max = s1 < s2 ? c2 : c1;
-        for (const auto& vertex : c_min)
+        auto size = vertices.size();
+        std::map<vertex, int> merged_candidates;
+        for (const auto& vertex : vertices)
         {
-            if (std::find_if(c_max.cbegin(), c_max.cend(), [&vertex] (const auto& v) { return vertex == v; }) != c_max.cend())
+            auto candidates = get_connected(vertex);
+            for (const auto& candidate : candidates)
             {
-                out.push_back(vertex);
+                merged_candidates[candidate]++;
+            }
+        }
+
+        vertex_array out = {};
+        for (const auto& pair : merged_candidates)
+        {
+            if (pair.second >= size)
+            {
+                out.push_back(pair.first);
             }
         }
         return out;
     }
 
+    inline std::uint64_t upper_bound(const clique& Q, const vertex_array& C)
+    {
+        return std::numeric_limits<std::uint64_t>::max();
+    }
+
     void max_clique(const clique& Q, const vertex_array& C)
     {
         auto ub = upper_bound(Q, C);
-        if (ub <= optimal_clique.m_heuristic_size) return;
-        if (ub <= optimal_clique.size()) return;
+        if (ub <= optimal_clique.m_vertices.size()) return;
+        if (ub <= m_heuristic_size) return;
         if (C.size() == 0)
         {
             optimal_clique = Q;
             return;
         }
 
+        auto elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(_chrono::now() - start_time);
+        if (elapsed.count() > time_limit)
+        {
+            throw std::runtime_error("Out of time");
+        }
+
         for (const auto& candidate : C)
         {
             auto temp_q = Q;
             temp_q.m_vertices.push_back(candidate);
-            vertex_array c_single(1, candidate);
-            // TODO: how to do intersection?
-//            auto new_c = intersection(c_single, C);
-            auto neighbours = find_candidates(candidate);
-            max_clique(temp_q, neighbours);
+            max_clique(temp_q, find_candidates(temp_q.m_vertices));
         }
+    }
+
+    std::string pretty_print(const clique& Q)
+    {
+        std::string s;
+        for (const auto& vertex : Q.m_vertices)
+        {
+            s.append(std::to_string(vertex));
+            s.append(" ");
+        }
+        return s;
     }
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[]) try
 {
-    using _chrono = std::chrono::steady_clock;
     if (argc < 2) return 1;
-    std::uint64_t time_limit = std::atoll(argv[2]);
+    time_limit = std::atof(argv[2]); // in seconds
+    if (time_limit == 0) return 1;
     std::ifstream f(argv[1]);
     if (!f.good()) return 1;
+
     std::string line;
     vertex n_vertices = 0;
     std::size_t n_edges = 0;
-    const char default_delim[] = " ";
-    auto start = _chrono::now();
+    static constexpr char default_delim[] = " ";
     while (!f.eof())
     {
         std::getline(f, line);
@@ -133,20 +161,22 @@ int main(int argc, char* argv[])
             adjacency_matrix[v2][v1]++;
         }
     }
-    auto end = _chrono::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
-    std::cout << "Parsing took " << elapsed.count() << " seconds" << std::endl;
 
-    start = _chrono::now();
+    start_time = _chrono::now();
     for (vertex v = 0; v < n_vertices; ++v)
     {
         clique q;
         q.m_vertices.push_back(v);
-        max_clique(q, find_candidates(v));
+        max_clique(q, get_connected(v));
     }
-    end = _chrono::now();
-    elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
-    std::cout << elapsed.count() << " q " << optimal_clique.size() << std::endl;
+    auto elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(_chrono::now() - start_time);
+    std::cout << elapsed.count() << " " << optimal_clique.m_vertices.size() << " " << pretty_print(optimal_clique) << std::endl;
 
     return 0;
+}
+catch (const std::exception&)
+{
+//    std::cout << time_limit << " " << optimal_clique.m_vertices.size() << " " << pretty_print(optimal_clique) << std::endl;
+    std::cout << time_limit << " 0 " << pretty_print(optimal_clique) << std::endl;
+    return 1;
 }
